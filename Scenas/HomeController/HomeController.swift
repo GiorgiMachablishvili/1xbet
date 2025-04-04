df91dd7b404148b76d9200108f8507fe5460cce8
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2025-04-04 11:28:20 +0400

    update

--- Scenas/HomeController/HomeController.swift
+++ Scenas/HomeController/HomeController.swift
@@ -11,10 +11,10 @@ class HomeController: UIViewController {
         .init(workoutTitle: "Swimming", workoutImage: "swimming", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
         .init(workoutTitle: "Treadmill", workoutImage: "treadmill", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
         .init(workoutTitle: "Swimming", workoutImage: "swimming", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
-        .init(workoutTitle: "Swimming", workoutImage: "swimming", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
+        .init(workoutTitle: "Ski", workoutImage: "ski", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
         .init(workoutTitle: "Treadmill", workoutImage: "treadmill", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
         .init(workoutTitle: "Swimming", workoutImage: "swimming", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
-        .init(workoutTitle: "Swimming", workoutImage: "swimming", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
+        .init(workoutTitle: "Ski", workoutImage: "ski", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm"),
         .init(workoutTitle: "Running outside", workoutImage: "run", workoutDistance: "658.83", workoutDuration: "3m25s", workoutData: "12:08 pm")
     ]
 
